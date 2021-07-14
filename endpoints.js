d7504cd7f485f484d1c2ecdeb1e4f1bf73ffa7ce
Igor Brito <britoigor77@gmail.com>
2021-07-14 18:15:19 -0300

    separando o serviço em funções

--- endpoints.js
+++ endpoints.js
@@ -21,18 +21,6 @@ function __formatDateToBD(dateString) {
 }
 
 
-var getDaysArray = function(start, end) {
-
-    end = new Date(end)
-
-
-    for(dt=new Date(start); dt<=end; dt.setDate(dt.getDate()+1)){
-        let nv_data = new Date(dt);
-        arr.push(nv_data.getDate()+'/'+(nv_data.getMonth()+1)+'/'+nv_data.getFullYear());
-    }
-    return arr;
-};
-
 
 // Retorna um array contendo as datas entre a data incial do investimento e a data atual
 function getDatesBetweenDates(startDate, endDate){
@@ -46,7 +34,6 @@ function getDatesBetweenDates(startDate, endDate){
 
         dates.push( dt.getFullYear()+'-'+(dt.getMonth()+1) +  ( parseInt(dt.getDate()) < 10 ?  `-0${dt.getDate()}` : `-${dt.getDate()}` ));
 
-
         dt.setDate(dt.getDate() + 1)
 
     }
@@ -56,11 +43,45 @@ function getDatesBetweenDates(startDate, endDate){
   }
   
 
+function calculo_cdb(cdbRate, dates, dados_cdi){
 
+    let valores_cdb = [];
+
+    let tcdi_k_acumulado = 1;
+ 
+    for (let data of dates){  
+ 
+
+        let dado_diario = dados_cdi.find( dado_cdi => {
+            return data === __formatDateToBD(dado_cdi.dtDate);
+        });
+ 
+ 
+        let cdi_diario,  tcdi_k;
+ 
+ 
+        if(dado_diario){
+ 
+            cdi_diario =  parseFloat(dado_diario.dLastTradePrice).toFixed(2);
+ 
+            tcdi_k=  parseFloat(Math.pow( parseFloat( parseFloat( cdi_diario/100 ) +1) , 1/252) -1).toFixed(8);
+ 
+            tcdi_k_acumulado = parseFloat( tcdi_k_acumulado *   parseFloat(1 +  parseFloat(tcdi_k * ( parseFloat(cdbRate/100) )  ) ) ).toFixed(8);
+ 
+ 
+            valores_cdb.push({date: data, data_csv: dado_diario.dtDate,  cdi_diario, unitPrice : tcdi_k, tcdi_k_acumulado})
+        }
+
+    }
+
+    return valores_cdb;
+}
 
 
 app.get('/cdb', async (req, res) => {
         
+    try{
+
       // #swagger.tags = ['User']
         // #swagger.description = 'Cáluco de CDB pós indexado ao CDI.'
 
@@ -82,50 +103,28 @@ app.get('/cdb', async (req, res) => {
                required: true
         } */
 
-   const investmentDate =   req.query.investmentDate;
-   const currentDate =      req.query.currentDate;
-   const cdbRate =          req.query.cdbRate;  
+        const investmentDate =   req.query.investmentDate;
+        const currentDate =      req.query.currentDate;
+        const cdbRate =          req.query.cdbRate;  
 
 
-   // adicionar validação de data e conversão pra padrão
-   
-   const dates = getDatesBetweenDates(investmentDate, currentDate);
-   
-   const dados_cdi = await neatCsv(fs.readFileSync('./CDI_Prices.csv')) // Lê o arquivo CSV da série histórica do CDI e o converte em um array de objetos 
+        // adicionar validação de data e conversão pra padrão
+        
+        const dates = getDatesBetweenDates( investmentDate, currentDate);
+        
+        const dados_cdi = await neatCsv(fs.readFileSync('./CDI_Prices.csv')) // Lê o arquivo CSV da série histórica do CDI e o converte em um array de objetos 
 
-   let valores_cdb = [];
+                
+        const result = calculo_cdb(cdbRate, dates, dados_cdi);
+        
 
-   let tcdi_k_acumulado = 1;
+        return res.status(200).send(result)
 
-   for (let data of dates){  
+    }
 
-
-       let dado_diario = dados_cdi.find( dado_cdi => {
-           return data === __formatDateToBD(dado_cdi.dtDate);
-       });
-
-
-       let cdi_diario,  tcdi_k;
-
-
-       if(dado_diario){
-
-           cdi_diario =  parseFloat(dado_diario.dLastTradePrice).toFixed(2);
-
-           tcdi_k=  parseFloat(Math.pow( parseFloat( parseFloat( cdi_diario/100 ) +1) , 1/252) -1).toFixed(8);
-
-           tcdi_k_acumulado = parseFloat( tcdi_k_acumulado *   parseFloat(1 +  parseFloat(tcdi_k * ( parseFloat(cdbRate/100) )  ) ) ).toFixed(8);
-
-
-           valores_cdb.push({date: data, data_csv: dado_diario.dtDate,  cdi_diario, unitPrice : tcdi_k, tcdi_k_acumulado})
-       }
-
-
-
-   }
-   
-
-    return res.status(200).send(valores_cdb)
+    catch(err){
+        return res.status(500).send({message: err.stack})
+    }
 
 })
 
